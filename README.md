1135ac1fe8351c5210cca817e3e1b7559e4f7fe5
lennypeers <lenny.peers@gmail.com>
2020-12-29 12:59:30 +0100

    cross browser update
    now compatible with Chrome

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # firefox-tabswitch
 
-extension that maps alt+{,shift}+space to {next,previous} tab.
+# general
+
+extension that maps `alt+{,shift}+space` to `{next,previous} tab`.
+
+This extension should be compatible with latest versions of Firefox and Chrome.
+
+# install
+
+- Firefox:
+	- download the xpi file in the Releases section
+
+- Chrome based browsers:
+	- clone the repository
+	- open `chrome://extensions/`
+	- turn the developper mode on (top right corner)
+	- load the module with `Load unpacked`
+
+# TODO
+- make it available on the extension stores.
--- foo.js
+++ foo.js
@@ -1,13 +1,17 @@
-browser.commands.onCommand.addListener(async function(command) {
-    const tabs = await browser.tabs.query({ currentWindow: true, hidden: false});
-	const sbat = await browser.tabs.query({currentWindow: true, active: true});
-	let current = sbat[0]
-	let currentIndex = tabs.findIndex(i => i.id === current.id);
-	if (command == "next") {
-		index = (currentIndex + 1) % tabs.length;
-	} else {
-		index = (currentIndex - 1 + tabs.length) % tabs.length;
-	}
-    const tab = tabs.slice(index);
-	browser.tabs.update(tab[0].id, { active: true });
+chrome.commands.onCommand.addListener(async function(command) {
+  chrome.tabs.query({ currentWindow: true}, function(tabs) {
+    chrome.tabs.query({currentWindow: true, active: true}, function(sbat) {
+      let current = sbat[0]
+      let currentIndex = tabs.findIndex(i => i.id === current.id);
+      if (command == "next") {
+        index = (currentIndex + 1) % tabs.length;
+      } else {
+        index = (currentIndex - 1 + tabs.length) % tabs.length;
+      }
+      const tab = tabs.slice(index);
+      chrome.tabs.update(tab[0].id, { active: true });
+    });
+  });
 });
+
+// vim: set ts=2 sts=2 sw=2 et:
